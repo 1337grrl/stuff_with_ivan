474d348f5206b97acc52f83ae60d95639a7791a0
Ivan Vasyliev <ivasyliev21@gmail.com>
2022-03-06 22:46:47 +0100

    Add font + debug text. Added pad + ball

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,82 @@
 #include <SFML/Graphics.hpp>
 
 
+sf::Vector2f padPos;
+float padSpeed = 0.1f;
+const sf::Vector2f padSize = sf::Vector2f(120.f, 10.f);
+const sf::Vector2f padHalfSize = padSize * 0.5f;
+
+
+sf::Vector2f ballPosition;
+float ballSpeed;
+float ballSize;
+
+sf::RectangleShape pad;
+sf::CircleShape ball;
+
+void ResetGame()
+{
+	ballPosition = sf::Vector2f(512.f, 500.f);
+	padPos = sf::Vector2f(512.f, 750.f);
+	ballSpeed = 0.05f;
+	ballSize = 10.f;
+}
+
+void Input()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		padPos.x -= padSpeed;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		padPos.x += padSpeed;
+	}
+	pad.setPosition(padPos);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	{
+		ResetGame();
+	}
+}
 
 int main()
 {
+	ResetGame();
+
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML works!");
 	
-	sf::Texture texture;
-	if (!texture.loadFromFile("content/brick_blue.png"))
+	sf::Texture brickTexture;
+	if (!brickTexture.loadFromFile("content/brick_blue.png"))
 	{
 		std::cout << "Error: unable to load texture";
 	}
 
-	sf::Sprite sprite = sf::Sprite(texture);
-	sprite.setPosition(sf::Vector2f(80.f, 80.f));
+	const int count = 5;
+	sf::Sprite sprites[count];
+	
+	for(int i = 0; i < count; ++i)
+	{
+		sprites[i] = sf::Sprite(brickTexture);
+		sprites[i].setPosition(sf::Vector2f(80.f + i * 150.f, 80.f));
+		sprites[i].setScale(0.2f, 0.2f);
+	}
+	
+
+	pad = sf::RectangleShape(padSize);
+	pad.setFillColor(sf::Color::Green);
+	//pad.setOrigin(sf::Vector2f(0.5f, 0.f));
+
+	ball = sf::CircleShape(ballSize);
+	ball.setFillColor(sf::Color::Magenta);
+	//ball.setOrigin(sf::Vector2f(0.5f, 0.f));
+	
+
+	sf::Font font;
+	if (!font.loadFromFile("content/font.ttf"))
+	{
+		// error...
+	}
 
 	while (window.isOpen())
 	{
@@ -28,11 +91,56 @@ int main()
 				window.close();
 		}
 
+		// --- Update ---
+
+		ballPosition.y += ballSpeed;
+		ball.setPosition(ballPosition);
+
+		if(ballPosition.y >= (padPos.y - ballSize))
+		{
+			if ((ballPosition.x <= (padPos.x + padSize.x)) && (ballPosition.x >= (padPos.x)))
+			{
+				ballSpeed = -ballSpeed;
+			}
+		}
+
+		Input();
+
+
+		// --- Render ---
 		sf::Color clearClr = sf::Color(0, 0, 0, 255);
 		//clearClr = sf::Color(128, 128, 128, 255);
 		window.clear(clearClr);
 		
-		window.draw(sprite);
+		for (int i = 0; i < count; ++i)
+		{
+			window.draw(sprites[i]);
+		}
+
+		window.draw(pad);
+		window.draw(ball);
+
+
+		// --- Debug ---
+
+		sf::Text text;
+
+		std::string debugText;
+		debugText = "Ball pos: ";
+		debugText += std::to_string(ballPosition.x);
+		debugText += "\n";
+		debugText +="Pad pos: ";
+		debugText += std::to_string(padPos.x);
+
+		text.setFont(font); // font is a sf::Font
+		text.setString(debugText.c_str());
+		text.setCharacterSize(24); // in pixels, not points!
+		text.setFillColor(sf::Color::Red);
+		text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+		window.draw(text);
+
+
+
 
 		window.display();
 	}
